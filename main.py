7cd952fde1b40050cb40f85ccb376b3ded0041f5
luccatsantos <ltanzillodos2023@fau.edu>
2025-04-30 22:25:13 -0400

    Update main.py

--- main.py
+++ main.py
@@ -92,6 +92,7 @@ def mandelbrot_img():
     return send_file(plot_both_sets(c), mimetype='image/png')
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    import os
+    port = int(os.environ.get("PORT", 8080))
     app.run(host='0.0.0.0', port=port)
 
